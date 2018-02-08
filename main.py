7eb9df783336f071df5a2976d41eb51c55446cc7
Chee Loong Ng <cheeloongng@Chees-MacBook-Pro.local>
2018-02-08 15:35:10 +0000

    update util

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def train_model():
     global model_columns, model
     model_columns, model = model_utils.train(df)
     
-    return 'success'
+    return 'success:from train_model'
 
 @app.route('/train_endpoint_without_file', methods=['GET'])
 def train_model_without_file():
@@ -50,7 +50,7 @@ def train_model_without_file():
     #joblib.dump(model, model_utils.MODEL_FILE_NAME)
     pickle.dump(model, open(model_utils.MODEL_FILE_NAME, 'wb'))
     
-    return 'success'
+    return 'success: from train_model_without_file'
 
 
 @app.route('/predict',methods=['POST'])
