928706a664152f35172bdb70d52c2ab3e52ac819
Chee Loong Ng <cheeloongng@Chees-MacBook-Pro.local>
2018-02-08 10:21:45 +0000

    created folders

---
+++ utils/model_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 10:08:29 2018
+
+@author: cheeloongng
+"""
+
+MODEL_DIR = 'model'
+MODEL_FILE_NAME = '%s/model.pkl' %(MODEL_DIR)
+DATA_FILE_PATH = 'data/'
+
+def train(df):
+    print('enter train')
+    
+    
+    return 'train'
+
+def predict(df,model):
+    print('enter predict')
+    return
+
+def update(df, model):
+    print('enter update')
+    return
